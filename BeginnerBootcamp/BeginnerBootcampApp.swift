bd24f33cf044bcd9a83e258c6e158d0795869209
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2023-04-27 22:53:33 -0700

    Initial Commit

---
+++ BeginnerBootcamp/BeginnerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginnerBootcampApp.swift
+//  BeginnerBootcamp
+//
+//  Created by Mohosin Islam Palash on 27/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BeginnerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeginnerBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeginnerBootcamp
+//
+//  Created by Mohosin Islam Palash on 27/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to Swift")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
