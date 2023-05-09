1c602d35a747cc9d2cc2a047374be915274fffa2
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2023-05-09 02:57:08 -0700

    SwiftUI Spacer

---
+++ BeginnerBootcamp/Views/Spacer.swift
@@ -0,0 +1,20 @@
+//
+//  Spacer.swift
+//  BeginnerBootcamp
+//
+//  Created by Mohosin Islam Palash on 9/5/23.
+//
+
+import SwiftUI
+
+struct Spacer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Spacer_Previews: PreviewProvider {
+    static var previews: some View {
+        Spacer()
+    }
+}
