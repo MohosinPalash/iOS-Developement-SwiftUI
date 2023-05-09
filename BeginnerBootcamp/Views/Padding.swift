33f76b1cdf63694bb1e49e060589303d6501e9cd
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2023-05-09 02:34:58 -0700

    SwiftUI Padding is added

---
+++ BeginnerBootcamp/Views/Padding.swift
@@ -0,0 +1,20 @@
+//
+//  Padding.swift
+//  BeginnerBootcamp
+//
+//  Created by Mohosin Islam Palash on 9/5/23.
+//
+
+import SwiftUI
+
+struct Padding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Padding_Previews: PreviewProvider {
+    static var previews: some View {
+        Padding()
+    }
+}
